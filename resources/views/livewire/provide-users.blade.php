b6f4c76731449ece0d480244b6f61bc091372d34
Patrick <patrick.obaf@gmail.com>
2020-11-02 11:17:44 +0100

    Add Digit Validation

--- resources/views/livewire/provide-users.blade.php
+++ resources/views/livewire/provide-users.blade.php
@@ -26,11 +26,11 @@
                                         </tr>
                                         <tr>
                                             <th scope="row">Name</th>
-                                            <td>{{ $helper->user['full_name'] }}</td>
+                                            <td>{{ $helper->user }}</td>
                                         </tr>
                                         <tr>
                                             <th scope="row"> Phone Number</th>
-                                            <td>{{ $helper->user['phone_number'] }}</td>
+                                            <td>{{ $helper->user }}</td>
                                         </tr>
                                     </tbody>
                                     </table>
