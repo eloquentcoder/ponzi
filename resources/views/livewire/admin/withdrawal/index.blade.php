579f7f5898f187894927ef9b5206b7f95c9eaf88
Patrick <patrick.obaf@gmail.com>
2020-11-02 04:59:12 +0100

    debug

--- resources/views/livewire/admin/withdrawal/index.blade.php
+++ resources/views/livewire/admin/withdrawal/index.blade.php
@@ -26,7 +26,7 @@
                                             <td>{{ $item->received == 0 ? 'Not Confirmed' : 'Confirmed' }}</td>
                                             <td>
                                                 @forelse ($item->providehelp as $item_help)
-                                                <li>{{ $item_help}}</li>
+                                                <li>{{ $item_help->user }}</li>
                                             @empty
                                                 No Merged Users
                                             @endforelse</td>
