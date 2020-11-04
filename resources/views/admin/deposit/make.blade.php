e4882cca174999cf62f8beafd415f71f3a0baca5
Patrick <patrick.obaf@gmail.com>
2020-11-04 02:41:37 +0100

    Updates Admin Invest

---
+++ resources/views/admin/deposit/make.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin-dashboard')
+
+@section('title', 'Make Investment')
+
+@section('content')
+
+    <livewire:admin.stat-boxes />
+
+    <livewire:make-investment />
+
+@endsection
