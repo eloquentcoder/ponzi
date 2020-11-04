32d371d590fe30498f973e42815af2e33f2c27e8
Patrick <patrick.obaf@gmail.com>
2020-11-04 09:18:45 +0100

    Updated Restriction Color

--- resources/views/user/restricted/index.blade.php
+++ resources/views/user/restricted/index.blade.php
@@ -10,7 +10,8 @@
     <div class="card m-b-30">
         <div class="card-body">
             <div class="card-title text-center">
-                <p style="color: red; font-weight: 900">Your account has been suspended.  Contact the admin through whatsapp on 09022209254 or send an email to customerservicegreenrichinvest@gmail.com.</p>
+                <p style="color: red; font-weight: 900">Your account has been suspended!
+                    Kindly contact GreenRich WhatsApp on +2349022209254. Or simply mail: customerservicegreenrichinvest@gmail.com</p>
                 <a href="#" class="btn btn-sm btn-success" onclick="event.preventDefault(); document.getElementById('logout-frm').submit();">Logout</a>
                 <form id="logout-frm" action="{{ route('logout') }}" method="POST" style="display: none;">
                     {{ csrf_field() }}
