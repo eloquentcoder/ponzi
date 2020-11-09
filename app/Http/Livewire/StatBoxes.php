d77f51666bc11960f58ab9102a03cd4e1e47aecc
Patrick <patrick.obaf@gmail.com>
2020-11-09 13:27:13 +0100

    Add User

--- app/Http/Livewire/StatBoxes.php
+++ app/Http/Livewire/StatBoxes.php
@@ -23,8 +23,8 @@ class StatBoxes extends Component
     public function render()
     {
         return view('livewire.stat-boxes', [
-            'total_investment' => auth()->user()->transactions()->where('type', 'provide_help')->sum('amount'),
-            'total_withdrawals' => auth()->user()->transactions()->where('type', 'get_help')->sum('amount'),
+            'total_investment' => auth()->user()->providehelp()->where('confirmed', 1)->sum('amount'),
+            'total_withdrawals' => auth()->user()->gethelp()->where('received', 1)->sum('amount'),
             'referral_bonus' => auth()->user()->value('referral_bonus'),
             'referrals' => auth()->user()->referrals()->count()
         ]);
