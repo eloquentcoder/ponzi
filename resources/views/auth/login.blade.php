4017d5cb4fcdc1bda86a20e3cd0d78c10b603495
Patrick <patrick.obaf@gmail.com>
2020-11-10 07:16:40 +0100

    Small Acount Chsngrs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,7 +58,7 @@
 
                 <div class="form-group">
                     <div class="col-sm-12 mt-4 text-center">
-                        <a href="{{ route('register') }}"> Don't Have an Account? <span style="color: blue">Sign up</span></a>
+                        <a href="{{ route('register') }}" style="font-weight: 800"> Need an Account? <span style="color: blue">Register Now</span></a>
                     </div>
                 </div>
 
