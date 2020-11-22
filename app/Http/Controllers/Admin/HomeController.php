640c401aeddb47663f145d245ce8a6843cfc6ad1
Patrick <patrick.obaf@gmail.com>
2020-11-22 11:58:10 +0100

    Massive End Updates

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\GetHelp;
 use App\Models\ProvideHelp;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\AddAdminRequest;
